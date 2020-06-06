a973d50b97c6441e3266130216bdc9cdaf6aa232
Scompa3 <scompa3@gmail.com>
2020-06-06 20:44:57 -0300

    Projeto Next Level Week

---
+++ public/scripts/intro.js
@@ -0,0 +1,36 @@
+// coments
+// document.write("Hello!")
+
+// variaveis, tipos de dados
+
+// var myvar = "Hello!"
+// const myconst = "He"
+
+// document.write(myvar + myconst)
+
+// boolean
+// const btrue = true
+// const bfalse = false
+
+// document.write(bfalse)
+
+// funçãoes / funcionalidades
+
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName("eu")
+
+// const notaFinal = 6
+
+// if(notaFinal >= 7){
+//   document.write("aprovou")
+// }else{
+//   document.write("reprovou")
+// }
+
+
+// for (i = 0; i < 10; i++){
+//   document.write(`<p> ${i} </p>`)
+// }
